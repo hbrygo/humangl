9cc4d0e6818e9daac4f32cb34540fc941b2a6630
Hugo Brygo <hubrygo@fu-r6-p5.s19.be>
2026-03-05 14:08:53 +0100

    debut casquette

--- Includes/include.hpp
+++ Includes/include.hpp
@@ -122,6 +122,8 @@ class body {
                     return parts[8].getAttachmentPoints();
                 case RIGHT_LOWER_LEG:
                     return parts[9].getAttachmentPoints();
+                case CAP:
+                    return parts[10].getAttachmentPoints();
                 default:
                     return {};
             }
